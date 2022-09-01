2d117a4328984af41e6f3f629c520c459e150a2d
AkashDangare911 <112281257+AkashDangare911@users.noreply.github.com>
2022-09-01 13:51:01 +0530

    Add files via upload

---
+++ 1_binarySearch.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[],int key,int size)
+{
+    int start=0;
+    int end=size-1;
+    int mid=start+(end-start)/2;
+
+    while(start<=end)
+    {
+        if(arr[mid]==key)
+        {
+            cout<<key<<" is Found at index : ";
+            return mid;
+        }
+        else
+        {
+            if(arr[mid]<key)
+            {
+                start=mid+1;
+            }
+            else
+            {
+                end=mid-1;
+            }
+        }
+        mid=start+(end-start)/2;
+    }
+    
+    return -1;
+}
+
+int main()
+{
+    int arr[]={1,2,3,4,5};
+    int s=sizeof(arr)/sizeof(arr[0]);
+    int key=4;
+
+    int ans=binarySearch(arr,key,5);
+
+    if(ans==-1){
+        cout<<"Element is not present in array . ";
+    }
+    else{
+        cout<<ans;
+    }
+    return 0;
+}
